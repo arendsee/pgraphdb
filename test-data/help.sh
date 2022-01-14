be96de075816744cf23a2f1f144d690d34ae6166
Zebulun Arendsee <zbwrnz@gmail.com>
2022-01-14 14:14:16 -0600

    Add test of help

---
+++ test-data/help.sh
@@ -0,0 +1,14 @@
+set -e
+set -u
+
+pgraphdb -h
+pgraphdb make -h
+pgraphdb start -h
+pgraphdb load -h
+pgraphdb query -h
+pgraphdb update -h
+pgraphdb construct -h
+pgraphdb ls_files -h
+pgraphdb ls_repo -h
+pgraphdb rm_data -h
+pgraphdb rm_repo -h
